c7e941b9fb68ae4a769678e0afc30168f5d32c6e
Karangwa Hirwa Julien <Karangwa Hirwa Julien>
2019-07-03 16:14:26 +0200

    feat(verification): add mail verification after signup
    - test the verification email method
    - add send verification method as helper
    - add verification method as controller
    
    [Finishes #166789869]

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -4,6 +4,7 @@ import userController from '../../controllers/userControllers';
 const router = express.Router();
 
 router.post('/users', userController.registerUser);
+router.post('/verification', userController.verification);
 router.post('/users/login', userController.login);
 router.post('/user/forgot-password', userController.forgotPassword);
 router.post('/user/reset-password/:userToken', userController.resetPassword);
